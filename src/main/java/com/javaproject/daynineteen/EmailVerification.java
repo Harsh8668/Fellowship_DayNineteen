ecd2152fa474329236ae88db84e3c568235d2318
Harsh8668 <choudharyharsh789@gmail.com>
2022-03-04 21:52:00 +0530

    Added the Day 19 Practice Problem

---
+++ src/main/java/com/javaproject/daynineteen/EmailVerification.java
@@ -0,0 +1,32 @@
+package com.javaproject.daynineteen;
+
+import java.util.regex.Pattern;
+
+public class EmailVerification {
+
+    public static void main(String[] args) {
+        EmailVerification emailVerification = new EmailVerification();
+
+        String [] validEmails = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com",
+                                "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au",
+                                "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+
+        String [] inValidEmails = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
+                                    ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com",
+                                    "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+        emailVerification.verification(validEmails);
+        emailVerification.verification(inValidEmails);
+    }
+
+    public void verification(String [] emails) {
+        for (int i=0; i < emails.length; i++){
+            boolean check = Pattern.matches("^[a-zA-Z]+[-\\+\\_\\.a-zA-Z0-9]+[@][a-zA-Z0-9]+([.]?[a-zA-Z\\,]{2,3}){2}", emails[i]);
+            if (check){
+                System.out.println("Email is Valid " + emails[i]);
+            }
+            else{
+                System.out.println("Email is InValid " + emails[i]);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/javaproject/daynineteen/UserRegistration.java
@@ -0,0 +1,79 @@
+package com.javaproject.daynineteen;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    static UserRegistration userRegistration = new UserRegistration();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {;
+        int option = 0;
+        while(option != 6){
+            option = userRegistration.showMenu();
+            userRegistration.userSelection(option);
+        }
+    }
+
+    public int showMenu(){
+        System.out.println("Choose an option : \n 1.  Validate First Name \n 2. Validate Last Name \n " +
+                "3. Validate Email \n 4. Validate Mobile Number \n 5. Validate Password \n " +
+                "6. Exit");
+        Scanner sc = new Scanner(System.in);
+        return (sc.nextInt());
+    }
+
+    public void userSelection(int option) {
+        switch (option){
+            case 1:
+                System.out.println("Enter the First Name");
+                userRegistration.validateFirstName(sc.next());
+                break;
+            case 2:
+                System.out.println("Enter the Last Name");
+                userRegistration.validateLastName(sc.next());
+                break;
+            case 3:
+                System.out.println("Enter the Email");
+                userRegistration.validateEmail(sc.next());
+                break;
+            case 4:
+                System.out.println("Enter the Mobile Number");
+                userRegistration.validateMobileNumber(sc.next());
+                break;
+            case 5:
+                System.out.println("Enter the Password");
+                userRegistration.validatePassword(sc.next());
+                break;
+            case 6:
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+    }
+
+    public void validateFirstName(String firstName) {
+        boolean valid = Pattern.matches("[A-Z]{1}[A-Za-z]+",firstName);
+        System.out.println(valid);
+    }
+
+    public void validateLastName(String lastName) {
+        boolean valid = Pattern.matches("[A-Z]{1}[A-Za-z]+",lastName);
+        System.out.println(valid);
+    }
+
+    public void validateEmail(String email) {
+        boolean valid = Pattern.matches("^[a-zA-Z]+[-\\+\\_\\.a-zA-Z0-9]+[@][a-zA-Z0-9]+([.]?[a-zA-Z\\,]{2,3}){2}",email);
+        System.out.println(valid);
+    }
+
+    public void validateMobileNumber(String mobile) {
+        boolean valid = Pattern.matches("[+91|0|941]?[9876][0-9]{9}", mobile);
+        System.out.println(valid);
+    }
+
+    public void validatePassword(String password) {
+        boolean valid = Pattern.matches("(.{8,}[A-Z0-9]?[+.$#@!&%*]?)", password);
+        System.out.println(valid);
+    }
+}
